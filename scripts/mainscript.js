c4bbef624ab689fd0426ebf6bc0a26a1886127f7
pleighsee08 <110692526+pleighsee08@users.noreply.github.com>
2022-08-05 19:47:51 -0500

    commented out old function, changed slashes to double backslash

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -1,6 +1,7 @@
-function testFunction() {
+/*function testFunction() {
     document.getElementById('demo').innerHTML = Date();
 }
+*/
 
 
 var i = 0; 			// Start Point
@@ -13,13 +14,13 @@ images[1] = "media\\abstract.PNG";
 images[2] = "media\\amberriver.PNG";
 images[3] = "media\\antiquerose.PNG";
 images[4] = "media\\autumnflames.PNG";
-images[5] = "media/bluebird.png";
-images[6] = "media/bluefeather.PNG";
-images[7] = "media/blueflower.PNG";
-images[8] = "media/dancinglady.PNG";
-images[9] = "media/dromedary.PNG";
-images[10] = "media/embers.PNG";
-images[11] = "media/fireworks.PNG";
+images[5] = "media\\bluebird.png";
+images[6] = "media\\bluefeather.PNG";
+images[7] = "media\\blueflower.PNG";
+images[8] = "media\\dancinglady.PNG";
+images[9] = "media\\dromedary.PNG";
+images[10] = "media\\embers.PNG";
+images[11] = "media\\fireworks.PNG";
 
 // Change Image
 function changeImg(){
